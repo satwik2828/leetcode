7af4f268aa702418587445eca4e1ba425011befc
satwik2828 <satwiktummala2015@gmail.com>
2025-12-10 19:48:00 +0530

    Time: 13 ms (48.56%) | Memory: 31.4 MB (20.51%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int,string>mp;
+        for(int i=0;i<heights.size();i++){
+                mp[heights[i]]=names[i];
+        }
+        sort(heights.rbegin(),heights.rend());
+        vector<string>res;
+        for(auto &i:heights){
+            res.push_back(mp[i]);
+        }
+        return res;
+    }
+};

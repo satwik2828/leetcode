2b59d92f11c3cb915a8740d12755df41c1df9b8e
satwik2828 <satwiktummala2015@gmail.com>
2025-12-10 20:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (45.37%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin=celsius + 273.15;
+        double Fahrenheit=celsius * 1.80 + 32.00;
+        vector<double>res;
+        res.push_back(kelvin);
+        res.push_back(Fahrenheit);
+        return res;
+    }
+};

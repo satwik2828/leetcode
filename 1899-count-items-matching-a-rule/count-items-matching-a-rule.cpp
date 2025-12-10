d4acfbfcc3783a15769c2f19d2b7f45d98339ff4
satwik2828 <satwiktummala2015@gmail.com>
2025-12-10 19:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (35.37%) - LeetSync

--- 1899-count-items-matching-a-rule/count-items-matching-a-rule.cpp
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        vector<string>type;
-        vector<string>color;
-        vector<string>name;
         int count=0;
-        for(auto &i:items){
-            type.push_back(i[0]);
-            color.push_back(i[1]);
-            name.push_back(i[2]);
-        }
         if(ruleKey=="color"){
-        for(int i=0;i<color.size();i++){
-            if(ruleValue==color[i]) count++;
+        for(int i=0;i<items.size();i++){
+            if(items[i][1]==ruleValue) count++;
         }
         }
         if(ruleKey=="type"){
-        for(int i=0;i<type.size();i++){
-            if(ruleValue==type[i]) count++;
+        for(int i=0;i<items.size();i++){
+            if(items[i][0]==ruleValue) count++;
         }
         }
         if(ruleKey=="name"){
-        for(int i=0;i<name.size();i++){
-            if(ruleValue==name[i]) count++;
+        for(int i=0;i<items.size();i++){
+            if(items[i][2]==ruleValue) count++;
         }
         }
         return count;

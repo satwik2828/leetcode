f626674149092eb2eee89c7b10974476b1aa507e
satwik2828 <satwiktummala2015@gmail.com>
2025-12-10 19:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (78.69%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -3,9 +3,12 @@ public:
     double minimumAverage(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         double minavg=DBL_MAX;
-        for(int i=0;i<nums.size()/2;i++){
-            double avg=(nums[i]+nums[nums.size()-i-1])/2.0;
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            double avg=(nums[i]+nums[j])/2.0;
             minavg=min(minavg,avg);
+            i++;
+            j--;
         }
         return minavg;
     }

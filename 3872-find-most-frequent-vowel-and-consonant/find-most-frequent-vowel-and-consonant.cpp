c79912dd06c7f1ad96e56f01c2c417bdf3c73851
satwik2828 <satwiktummala2015@gmail.com>
2025-12-12 11:36:32 +0530

    Time: 3 ms (35.44%) | Memory: 9.3 MB (86.51%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-     map<char,int>mp;
+     vector<int>mp(26,0);
      int vc=0,cc=0;
      for(char ch: s){
-        mp[ch]++;
+        mp[ch-'a']++;
      }
-     for(char ch='a';ch<='z';ch++){
-        if(ch=='a' ||ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-            vc=max(vc,mp[ch]);
+     for(int i=0;i<26;i++){
+        if(i==0 ||i==4 ||i==8 || i==14 ||i==20){
+            vc=max(vc,mp[i]);
         }
         else{
-            cc=max(cc,mp[ch]);
+            cc=max(cc,mp[i]);
         }
      }
      return vc+cc;

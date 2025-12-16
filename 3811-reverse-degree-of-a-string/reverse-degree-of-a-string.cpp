5bde4d696b6b385d2aa454cf4b3076935b4ec21f
satwik2828 <satwiktummala2015@gmail.com>
2025-12-16 11:13:46 +0530

    Time: 183 ms (6.58%) | Memory: 9.9 MB (16.80%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        vector<int>v(27,0);
-        int n=v.size();
-        for(int i=1;i<n;i++){
-            v[i]=i;
-        }
         int sum=0;
         for(int i=0;i<s.length();i++){
-            sum+=(i+1)*(n-v[s[i]-'a']-1);
+            sum+=(i+1)*(26-(s[i]-'a'));
             cout<<sum<<endl;
         }
     return sum;

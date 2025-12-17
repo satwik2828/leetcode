4c3d557730ccd9b813adf1929e65c45a9e1b53ec
satwik2828 <satwiktummala2015@gmail.com>
2025-12-17 10:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (40.86%) - LeetSync

--- 2304-cells-in-a-range-on-an-excel-sheet/cells-in-a-range-on-an-excel-sheet.cpp
+++ 2304-cells-in-a-range-on-an-excel-sheet/cells-in-a-range-on-an-excel-sheet.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     vector<string> cellsInRange(string s) {
     vector<string>res;
+    int start=s[1]-'0';
+    int end=s[4]-'0';
     for(char i=s[0];i<=s[3];i++){
-        for(int j=s[1]-'0';j<=s[4]-'0';j++){
+        for(int j=start;j<=end;j++){
             res.push_back(i+to_string(j));
         }
     }

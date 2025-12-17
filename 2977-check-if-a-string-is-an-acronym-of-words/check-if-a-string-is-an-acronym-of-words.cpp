1bd172ab3d947eb09a1e510a7b90d9acef48e1b2
satwik2828 <satwiktummala2015@gmail.com>
2025-12-17 15:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (58.47%) - LeetSync

--- 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAcronym(vector<string>& words, string s) {
         string comp;
-        for(string word : words){
+        for(string & word : words){
             comp.push_back(word[0]);
         }
         return comp==s;

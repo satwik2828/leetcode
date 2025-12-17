d5ab3463c257867074e01e324173f3572fcdf244
satwik2828 <satwiktummala2015@gmail.com>
2025-12-17 11:52:17 +0530

    Time: 7 ms (35.26%) | Memory: 26.7 MB (39.82%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    string firstPalindrome(vector<string>& words) {
-        vector<string>compare;
-        for(int i=0;i<words.size();i++){
-            string temp=words[i];
-            reverse(temp.begin(),temp.end());
-            compare.push_back(temp);
+    bool is_palindrome(string s){
+        int n=s.length();
+        for(int i=0;i<n/2;i++){
+            if(s[i]!=s[n-i-1]){
+                return false;
+            }
         }
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        
         for(int i=0;i<words.size();i++){
-            if(words[i]==compare[i]){
+            if(is_palindrome(words[i])){
                 return words[i];
-                break;
             }
         }
         return "";

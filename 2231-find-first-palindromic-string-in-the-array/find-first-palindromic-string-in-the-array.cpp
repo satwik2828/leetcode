f6e1c2f42db5666aff22fe4f76d92cf3806367e7
satwik2828 <satwiktummala2015@gmail.com>
2025-12-17 11:55:26 +0530

    Time: 8 ms (27.69%) | Memory: 26.7 MB (29.64%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool is_palindrome(string s){
-        int n=s.length();
+        int n=s.size();
         for(int i=0;i<n/2;i++){
             if(s[i]!=s[n-i-1])  return false;
         }

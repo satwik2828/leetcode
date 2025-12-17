f176f22bebbef370e43d77d0deb9d5c3cd6f3e90
satwik2828 <satwiktummala2015@gmail.com>
2025-12-17 14:49:08 +0530

    Time: 15 ms (63.08%) | Memory: 15.9 MB (69.12%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    bool isvowel(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            return true;
+        }
+        return false;
+    }
     string sortVowels(string s) {
         vector<char>temp;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                if(isvowel(s[i])){
                 temp.push_back(s[i]);
-            }
+                }
         }
         sort(temp.begin(),temp.end());
         int k=0;
         for(int i=0;i<s.length();i++){
-             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+             if(isvowel(s[i])){
                 s[i]=temp[k++];
             }
         }

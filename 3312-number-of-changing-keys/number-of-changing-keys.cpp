0d053401d27e31cd1608a809d7b488b93d59d79f
satwik2828 <satwiktummala2015@gmail.com>
2025-12-19 11:33:47 +0530

    Time: 2 ms (12.52%) | Memory: 8.9 MB (80.75%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int count=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s[i+1]!=tolower(s[i]) && s[i+1]!=toupper(s[i]) && s[i]!=s[i+1]) count++; 
+        }
+        return count;
+    }
+};

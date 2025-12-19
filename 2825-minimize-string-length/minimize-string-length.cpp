cc8892987f687cae825268d1707ed7599ce08f69
satwik2828 <satwiktummala2015@gmail.com>
2025-12-19 15:33:37 +0530

    Time: 1 ms (94.06%) | Memory: 12.9 MB (76.23%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.cpp
+++ 2825-minimize-string-length/minimize-string-length.cpp
@@ -3,11 +3,11 @@ public:
     int minimizedStringLength(string s) {
         vector<int>ref(26,0);
         for(char &ch:s){
-            ref[ch-'a']++;
+            ref[ch-'a']=1;
         }
         int count=0;
-        for(int &i:ref){
-            if(i>0) count++;
+        for(int i:ref){
+            if(i==1) count++;
         }
         return count;
     }

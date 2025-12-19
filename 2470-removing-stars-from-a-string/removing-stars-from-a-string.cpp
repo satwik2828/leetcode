e8dab5e0d79327464e78880c836ac66faa5c7812
satwik2828 <satwiktummala2015@gmail.com>
2025-12-19 15:03:28 +0530

    Time: 15 ms (74.94%) | Memory: 28.5 MB (50.05%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     string removeStars(string s) {
-        vector<char>res;
+        string res;
         for(int i=0;i<s.length();i++){
             if(s[i]!='*') res.push_back(s[i]);
             else res.pop_back();
         }
-        string ans="";
-        for(char ch: res) ans+=ch;
-        return ans;
+        return res;
     }
 };

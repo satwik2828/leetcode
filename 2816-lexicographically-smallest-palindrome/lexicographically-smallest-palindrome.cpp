0d0e012b4ebf9bdf94239859b7d7961d7ae47f15
satwik2828 <satwiktummala2015@gmail.com>
2025-12-19 10:14:24 +0530

    Time: 4 ms (67.45%) | Memory: 18.5 MB (62.60%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.length();
         for(int i=0;i<n/2;i++){
             if(s[n-i-1]>s[i]) s[n-i-1]=s[i];
-            if(s[n-i-1]<s[i]) s[i]=s[n-i-1];
+            else s[i]=s[n-i-1];
         }
         return s;
     }

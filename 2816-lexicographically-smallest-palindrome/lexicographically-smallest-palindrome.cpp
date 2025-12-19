a02f3f466b2ffad4aedbb30af8823636b6f6b974
satwik2828 <satwiktummala2015@gmail.com>
2025-12-19 10:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (62.60%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string makeSmallestPalindrome(string s) {
         int n=s.length();
-        for(int i=0;i<n/2;i++){
+        for(int i=0;i<n;i++){
             if(s[n-i-1]>s[i]) s[n-i-1]=s[i];
             else s[i]=s[n-i-1];
         }

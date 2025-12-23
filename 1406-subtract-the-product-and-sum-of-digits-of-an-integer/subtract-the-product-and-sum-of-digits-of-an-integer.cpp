79ef124b22deed66b10067da6735a916c26973d9
satwik2828 <satwiktummala2015@gmail.com>
2025-12-23 14:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.16%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int sum=0,pro=1;
+        while(n>0){
+            int digit=n%10;
+            sum+=digit;
+            pro*=digit;
+            n=n/10;
+        }
+        return pro-sum;
+    }
+};

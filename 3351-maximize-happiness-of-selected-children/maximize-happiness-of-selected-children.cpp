e30871e435e53e9217a2255a5d59f787224b5083
satwik2828 <satwiktummala2015@gmail.com>
2025-12-25 19:19:43 +0530

    Time: 165 ms (41.14%) | Memory: 108 MB (47.16%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -4,11 +4,10 @@ public:
         long long sum=0;
         int n=happiness.size();
         sort(happiness.begin(),happiness.end());
-        for(int i=0;i<n && k>0;i++){
+        for(int i=0;i<k;i++){
             int val=happiness[n-i-1]-i;
             if(val < 0) val=0;
             sum+=val;
-            k--;
         }
         return sum;
     }

ea1854ba1772a60613ba254b43cd1c07d189da25
satwik2828 <satwiktummala2015@gmail.com>
2025-12-28 19:32:25 +0530

    Time: 4 ms (2.12%) | Memory: 28.8 MB (11.13%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int Single_sum=0,Double_sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(floor(log10(nums[i])+1)==2) Double_sum+=nums[i];
+            else Single_sum+=nums[i];
+        }
+        cout<<Single_sum<<Double_sum;
+        return Single_sum!=Double_sum;
+    }
+};

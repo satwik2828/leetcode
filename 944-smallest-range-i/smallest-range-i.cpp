a780ed244348b264c073a75049b5e19b629b4c09
satwik2828 <satwiktummala2015@gmail.com>
2025-12-30 12:04:56 +0530

    Time: 3 ms (8.21%) | Memory: 19.3 MB (57.58%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.cpp
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
+        int n=nums.size();
         sort(nums.begin(),nums.end());
-        int ans=nums[nums.size()-1]-nums[0]-(2*k);
-    if(ans<=0) return 0;
-    else return ans;
+        int ans=nums[n-1]-nums[0]-(2*k);
+        if(ans<=0) return 0;
+        else return ans;
     }
 };

884bde1d52fed71960126f7d6fba97859629ff4e
satwik2828 <satwiktummala2015@gmail.com>
2026-01-06 09:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (37.17%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.cpp
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.cpp
@@ -3,7 +3,7 @@ public:
     int maximumStrongPairXor(vector<int>& nums) {
         int maximum=INT_MIN;
         for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
+            for(int j=i;j<nums.size();j++){
                 if(abs(nums[i]-nums[j])<=min(nums[i],nums[j])){
                     maximum=max(maximum,nums[i]^nums[j]);
                 }

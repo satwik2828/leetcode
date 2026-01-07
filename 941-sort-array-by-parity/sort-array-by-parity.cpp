7d3ae73a7966352af82c514e817cbdc2e2cafb83
satwik2828 <satwiktummala2015@gmail.com>
2026-01-07 22:56:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (15.50%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -4,10 +4,10 @@ public:
        vector<int>ans;
        int n=nums.size();
        for(int i=0;i<n;i++){
-        if(!(nums[i]&1)) ans.push_back(nums[i]);
+        if(nums[i]%2==0) ans.push_back(nums[i]);
        }
        for(int i=0;i<n;i++){
-        if(nums[i]&1) ans.push_back(nums[i]);
+        if(nums[i]%2!=0) ans.push_back(nums[i]);
        }
        return ans;
     }

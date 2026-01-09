32f1ed5622d2794ab2dc7aeee0b4c7d88f869120
satwik2828 <satwiktummala2015@gmail.com>
2026-01-09 14:16:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (35.17%) - LeetSync

--- 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.cpp
+++ 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.cpp
@@ -3,7 +3,6 @@ public:
     long long findTheArrayConcVal(vector<int>& nums) {
         int  n=nums.size();
         long long sum=0;
-        
         for(int i=0;i<n/2;i++){
             sum+=stoll(to_string(nums[i])+to_string(nums[n-i-1]));
         }

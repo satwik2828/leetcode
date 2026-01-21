a8db01dfb735d305fbcb450eb5434ea91b3935a5
satwik2828 <satwiktummala2015@gmail.com>
2026-01-21 15:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.76%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -3,14 +3,12 @@ public:
     string reverseOnlyLetters(string s) {
         int n=s.length();
         int l=0,r=n-1;
-        while(l<=r){
-            while(r>=0 and !isalpha(s[r])) r--;
-            while(l<s.size() and !isalpha(s[l])) l++;
-            if(l<s.size() and l<=r and isalpha(s[l]) && isalpha(s[r])){
-                swap(s[l],s[r]);
-                r--;
-                l++;
-            }
+        while(l<r){
+            while(!isalpha(s[l]) && l<r) l++;
+            while(!isalpha(s[r]) && l<r) r--;
+            swap(s[l],s[r]);
+            l++;
+            r--;
         }
         return s;
     }

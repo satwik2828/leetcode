99b52b22c1f6f1550699169eb6e3da90df2bfb93
satwik2828 <satwiktummala2015@gmail.com>
2026-01-29 10:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (17.79%) - LeetSync

---
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        for(int i=0;i<score.size();i++){
+                swap(score[i][0],score[i][k]);
+        }
+        sort(score.rbegin(),score.rend());
+         for(int i=0;i<score.size();i++){
+                swap(score[i][0],score[i][k]);
+        }
+        return score;
+    }
+};

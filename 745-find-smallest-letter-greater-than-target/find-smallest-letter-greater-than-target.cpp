75da70284a3a1e96365660d9845e7a81a3b8f8ac
satwik2828 <satwiktummala2015@gmail.com>
2026-01-31 11:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (62.61%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -8,7 +8,6 @@ public:
         if(letters[mid]<=target) left=mid+1;
         else right=mid-1;
         }
-        cout<<left<<right<<endl;
        if(letters[left]<=target && left==letters.size()-1) return ans;
        if(letters[left]<=target) return letters[left+1];
        return letters[left];

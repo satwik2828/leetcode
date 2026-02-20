2bfdc3472ec7d1fb4d0184939223206631581675
satwik2828 <satwiktummala2015@gmail.com>
2026-02-20 10:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 33.9 MB (96.35%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
        map<int,int>mp;
+       int maxfreq=0;
        for(int i:nums){
         mp[i]++;
+        maxfreq=max(maxfreq,mp[i]);
        }
-       vector<vector<int>>ans;
-       while(true){
-        vector<int>temp;
-        bool added=0;
-       for(auto & i: mp){
-        if(i.second>0){ 
-           temp.push_back(i.first);
-            i.second--;
-            added=1;
+       vector<vector<int>>ans(maxfreq);
+     for(auto & p : mp){
+        for(int i=0;i<p.second;i++){
+            ans[i].push_back(p.first);
         }
-       }
-        if(added==0) break;
-       ans.push_back(temp);
-       }
+     }
     return ans;
     }
 };

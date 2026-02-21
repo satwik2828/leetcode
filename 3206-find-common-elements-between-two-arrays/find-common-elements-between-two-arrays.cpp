da543126303aae2f02daba1dceb4bab617a553f2
satwik2828 <satwiktummala2015@gmail.com>
2026-02-21 10:07:53 +0530

    Time: 18 ms (31.63%) | Memory: 72.1 MB (5.00%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
         int c1=0,c2=0;
         unordered_map<int,int>mp1,mp2;
-        for(auto & i:nums1) mp1[i]++;
-        for(auto & i:nums2) mp2[i]++;
-        for(auto &i:nums1){
+        for(auto  i:nums1) mp1[i]++;
+        for(auto  i:nums2) mp2[i]++;
+        for(auto i:nums1){
             if(mp2[i]>0) c1++;
         }
-        for(auto &i:nums2){
+        for(auto i:nums2){
             if(mp1[i]>0) c2++;
         }
         return {c1,c2};

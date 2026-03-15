e87c4aae1b57e0a9981be15279706258e4a8abe9
satwik2828 <satwiktummala2015@gmail.com>
2026-03-15 07:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (94.14%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
-    int ftw(vector<int>a,int k,int idx){
-        if(a.size()==1) return a[0];
-        idx=(idx+k-1)%a.size();
-        a.erase(a.begin()+idx);
-        return ftw(a,k,idx);
-    }
     int findTheWinner(int n, int k) {
-        int idx=0;
-        vector<int>ls;
-        for(int i=1;i<=n;i++) ls.push_back(i);
-        return ftw(ls,k,idx);
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=(ans+k)%i;
+        }
+        return ans+1;
     }
 };

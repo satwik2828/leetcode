9434a14021ffd60ace7db865cdd741140a9771b4
satwik2828 <satwiktummala2015@gmail.com>
2026-07-09 22:04:51 +0530

    Time: 3 ms (89.05%) | Memory: 23.8 MB (59.56%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>>ans;
         ans.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
+            if(intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
             else ans.back()[1]=max(ans.back()[1],intervals[i][1]); 
         }
         return ans;

9dfb54b31829e413609b85664e329ad0202d6077
satwik2828 <satwiktummala2015@gmail.com>
2026-07-12 07:54:18 +0530

    Time: 19 ms (98.25%) | Memory: 40.5 MB (80.59%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,int>mp;
-        for(int i:arr) mp[i];
-        int k=1;
-        for(auto &i:mp){
-            i.second=k;
-            k++;
+        if(arr.empty()) return {};
+        vector<int>temp=arr;
+        vector<pair<int,int>>mp;
+        for(int i=0;i<arr.size();i++){
+            mp.push_back({temp[i],i});
         }
-        vector<int>ans;
-        for(int i:arr){
-            ans.push_back(mp[i]);
+         sort(mp.begin(),mp.end());
+        vector<int>ans(arr.size());
+        ans[mp[0].second]=1;
+        for(int i=1;i<arr.size();i++){
+            if(mp[i].first==mp[i-1].first) ans[mp[i].second]=ans[mp[i-1].second];
+            else ans[mp[i].second]=ans[mp[i-1].second]+1;
         }
         return ans;
     }

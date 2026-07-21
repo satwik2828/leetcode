37e2c4eb759c0fa56155759116d4c4ca083366aa
satwik2828 <satwiktummala2015@gmail.com>
2026-07-21 11:57:56 +0530

    Time: 4 ms (12.90%) | Memory: 9.6 MB (50.05%) - LeetSync

--- 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
+++ 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     string rearrangeString(string s, char x, char y) {
-        string s1,s2,s3;
+        string s1,s2;
         for(char ch:s){
-            if(ch==x) s1.push_back(ch);
-            else if(ch==y) s3.push_back(ch);
+            if(ch==y) s1.push_back(ch);
             else s2.push_back(ch);
         }
-        return s3+s2+s1;
+        return s1+s2;
     }
 };

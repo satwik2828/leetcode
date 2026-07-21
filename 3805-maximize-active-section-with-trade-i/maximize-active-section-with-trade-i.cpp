35973fcba2489c34b064b122d6951366e476b3d4
satwik2828 <satwiktummala2015@gmail.com>
2026-07-21 21:45:22 +0530

    Time: 115 ms (25.22%) | Memory: 80.8 MB (37.39%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n=s.length();
+        vector<int>a;
+        int s_count=0,count=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1') s_count++;
+        }
+        if(s.size()<=2 || s_count==n) return s_count;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                count++;
+            }
+            else{
+                if(count>0){
+                a.push_back(count);
+                count=0;
+                }
+            }
+        }
+        if(count>0) a.push_back(count);
+        int c=0;
+        for(int i=0;i<a.size()-1;i++){
+            c=max(c,a[i]+a[i+1]);
+        }
+        cout<<c<<" "<<s_count;
+        return s_count+c;
+    }
+};

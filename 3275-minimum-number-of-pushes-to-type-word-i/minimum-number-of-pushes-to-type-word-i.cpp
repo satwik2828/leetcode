b2bc573cdff7133f024206496ba25837271fddf5
satwik2828 <satwiktummala2015@gmail.com>
2026-07-30 07:17:39 +0530

    Time: 3 ms (11.73%) | Memory: 8.3 MB (54.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n=word.length();
+        int k=n/8;//nth_term_of_ap
+        int remaining=(k+1)*(n%8);//remaining at last
+        int sum=4*k + 4*k*k;//sum_till_nth_term
+        cout<<sum<<" "<<remaining;
+        return sum+remaining;
+    }
+};

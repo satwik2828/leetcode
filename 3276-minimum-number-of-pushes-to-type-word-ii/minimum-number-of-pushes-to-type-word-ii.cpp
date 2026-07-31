a5b726223b5d8e533deca9f930af5e2037dca21b
satwik2828 <satwiktummala2015@gmail.com>
2026-07-31 07:23:32 +0530

    Time: 6 ms (75.61%) | Memory: 25.4 MB (84.45%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,17 +6,9 @@ public:
         for(int i=0;i<n;i++){
             mp[word[i]-'a']++;
         }
-        vector<pair<char,int>>vp;
-        for(int i=0;i<mp.size();i++){
-            vp.push_back({i+'a',mp[i]});
-        }
-         sort(vp.begin(),vp.end(),[](const auto &a,const auto &b){
-             return a.second>b.second;
-            });
-        int n_count=0;
-        for(int i=0;i<26;i++){
-            if(vp[i].second==0) break;
-            count+=((i/8)+1)*vp[i].second;
+         sort(mp.begin(),mp.end(),greater<int>());
+        for(int i=0;i<26 && mp[i]>0;i++){
+           count+=((i/8)+1)*mp[i];
         }
         return count;
     }

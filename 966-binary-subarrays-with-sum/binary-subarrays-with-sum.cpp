22cfad4154c83dd183be6d4eb9b0076bd813f23c
satwik2828 <satwiktummala2015@gmail.com>
2026-07-31 20:56:46 +0530

    Time: 2 ms (57.50%) | Memory: 34.8 MB (35.29%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -3,13 +3,12 @@ public:
     int sw(vector<int>nums,int g){
         int sum=0,n=nums.size();
         int left=0,right=0,count=0;
-       while(right<n){
+       for(right=0;right<n;right++){
         sum+=nums[right];
         while(sum>g && left<=right){
             sum-=nums[left++];
         }
         count+=right-left+1;
-        right++;
        } 
        return count;
     }

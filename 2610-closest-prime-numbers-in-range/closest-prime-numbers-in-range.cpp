3c7ef90908c67e2728b2e1cf6f20302103e86eed
satwik2828 <satwiktummala2015@gmail.com>
2026-08-26 12:19:54 +0530

    Time: 571 ms (18.24%) | Memory: 23.3 MB (30.17%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
    vector<bool>primes;
-   int n=1000000;
-   void sieve(){
-    primes.resize(n+1);
-    for(int i=2;i<=n;i++) primes[i]=true;
-    for(int i=2;i*i<=n;i++){
-        for(int j=i*i;j<=n;j+=i){
+   void sieve(int right){
+    primes.resize(right+1);
+    for(int i=2;i<=right;i++) primes[i]=true;
+    for(int i=2;i*i<=right;i++){
+        for(int j=i*i;j<=right;j+=i){
             primes[j]=false;
         }
     }
    }
     vector<int> closestPrimes(int left, int right) {
-        sieve();
+        sieve(right);
         int mini=INT_MAX;
         vector<int>ref;
         for(int i=left;i<=right;i++){

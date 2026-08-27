0007dbfd0975ab378c5318ea11c08d3405949fab
satwik2828 <satwiktummala2015@gmail.com>
2026-08-27 07:45:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (83.14%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-      int k=s.length()-1;
-      int l=goal.length()-1;
-      if(k!=l) return false;
-      string temp=s;
-      while(k>=0){
-        reverse(temp.begin(),temp.end());
-        reverse(temp.begin(),temp.begin()+k);
-        reverse(temp.begin()+k,temp.end());
-        cout<<temp;
-        if(temp==goal){
-            return true;
+    //   int k=s.length()-1;
+    //   int l=goal.length()-1;
+    //   if(k!=l) return false;
+    //   string temp=s;
+    //   while(k>=0){
+    //     reverse(temp.begin(),temp.end());
+    //     reverse(temp.begin(),temp.begin()+k);
+    //     reverse(temp.begin()+k,temp.end());
+    //     cout<<temp;
+    //     if(temp==goal){
+    //         return true;
+    //     }
+    //     else{
+    //         temp=s;
+    //         k--;
+    //     }
+    //   }
+    //   return false;
+    if(s.size()!=goal.size()) return false;
+        for(int i=0;i<s.size();i++){
+            char ad=s[0];
+            s.erase(0,1);
+            s+=ad;
+            if(s==goal) return true;
         }
-        else{
-            temp=s;
-            k--;
-        }
-      }
-      return false;
+        return false;
     }
 };

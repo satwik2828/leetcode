55224ae32b22fbbeb35f8948f43b2d8bcb517d37
satwik2828 <satwiktummala2015@gmail.com>
2026-08-28 19:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (28.05%) - LeetSync

--- 3026-find-the-minimum-possible-sum-of-a-beautiful-array/find-the-minimum-possible-sum-of-a-beautiful-array.cpp
+++ 3026-find-the-minimum-possible-sum-of-a-beautiful-array/find-the-minimum-possible-sum-of-a-beautiful-array.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int minimumPossibleSum(int n, int target) {
-        long long k=1,sum=0,l=target/2,a=1e9+7;
-        while(k<=l && k<=n){
-            sum+=k;
-            k++;
-        }
-        cout<<k<<endl;
-        int m=n-k+1;
-        cout<<m<<" "<<sum<<endl;
+        long long k=min(target/2,n),sum=0,a=1e9+7;
+        // while(k<=l && k<=n){
+        //     sum+=k;
+        //     k++;
+        // }
+        sum+=(1ll*(k*(k+1)/2))%a;
+        // cout<<k<<endl;
+        int m=n-k;
+        // cout<<m<<" "<<sum<<endl;
         sum+=(1ll*(1ll*target*2+(m-1))*m/2)%a;
         // cout<<(m%a*(m%a-1)/2)%a<<endl;
         return sum%(a);

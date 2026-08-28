1365807e9792b70f5ab25df44ae345386dcb643d
satwik2828 <satwiktummala2015@gmail.com>
2026-08-28 11:57:09 +0530

    Time: 3 ms (78.66%) | Memory: 39.5 MB (71.26%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.cpp
+++ 2722-prime-in-diagonal/prime-in-diagonal.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    vector<bool>primes;
-    void seive(int maxi){
-        primes.resize(maxi+1);
-        for(int i=2;i<=maxi;i++) primes[i]=true;
-        for(int i=2;i*i<=maxi;i++){
-            for(int j=i*i;j<=maxi;j+=i){
-                primes[j]=false;
-            }
+    bool isprime(int x){
+        if(x<2) return false;
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0) return false;
         }
+        return true;
     }
     int diagonalPrime(vector<vector<int>>& nums) {
-        int n=nums.size(),ans=0,maxi=INT_MIN;
+        int n=nums.size(),ans=0;
         for(int i=0;i<n;i++){
-           maxi=max(maxi,max(nums[i][i],nums[i][n-i-1]));
-        }
-        seive(maxi);
-        for(int i=0;i<n;i++){
-            if(primes[nums[i][i]]) ans=max(ans,nums[i][i]);
-            if(primes[nums[i][n-i-1]]) ans=max(ans,nums[i][n-i-1]);
+            if(isprime(nums[i][i]))  ans=max(ans,nums[i][i]);
+            if(isprime(nums[i][n-i-1]))  ans=max(ans,nums[i][n-i-1]);
         }
         return ans;
     }

50a5a3447f4410722a91ba0c16a5ded97454a7e9
satwik2828 <satwiktummala2015@gmail.com>
2026-09-03 06:41:24 +0530

    Time: 3 ms (72.51%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n=nums1.size();
-        int no=0,ne=0,higho=INT_MAX,highe=INT_MAX;
+        // int no=0,ne=0,higho=INT_MAX,highe=INT_MAX;
+        // for(int i:nums1){
+        //     if(i&1) {
+        //         no++;
+        //         higho=min(higho,i);
+        //     }
+        //     else {
+        //         ne++;
+        //         highe=min(highe,i);
+        //     }
+        // }
+        // if(no!=n && ne!=n){
+        //     if(higho>=highe) return false; 
+        // }
+        // return true;
+        int mini=*min_element(nums1.begin(),nums1.end());
+        if(mini&1) return true;
         for(int i:nums1){
-            if(i&1) {
-                no++;
-                higho=min(higho,i);
-            }
-            else {
-                ne++;
-                highe=min(highe,i);
-            }
-        }
-        if(no!=n && ne!=n){
-            if(higho>=highe) return false; 
+            if(i&1) return false;
         }
         return true;
-    }
+    }//idi kuda ekkuve array lo eppudu minimum element odd avutene true and lekapothe anni evens vundali
 };

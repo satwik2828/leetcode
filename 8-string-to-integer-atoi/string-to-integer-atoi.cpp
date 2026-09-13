e14ff1a733e7256d1b69dba94e5ac23230f2b75f
satwik2828 <satwiktummala2015@gmail.com>
2026-09-13 11:01:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (5.40%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,60 +1,26 @@
 class Solution {
 public:
+    int solve(string s,int i,int sign,long long ans)
+    {
+        if(i>=s.size() || s[i]<'0' || s[i]>'9') return ans*sign;
+        int digit=s[i]-'0';
+        if(ans>(INT_MAX-digit)/10){
+            if(sign==1)   return INT_MAX;
+            else return INT_MIN;
+        }
+        ans=ans*10+digit;
+        return solve(s,i+1,sign,ans);
+    }
     int myAtoi(string s) {
-        string number="";
-        char sign='+';
-        int ncount=0,pcount=0;
-        int i=0,flag=0,n=s.length();
-        while(i<n){
-            if(isalpha(s[i])) return 0;
-            if(s[i]=='.') break;
-            if(s[i]=='+' && flag!=1) {
-                pcount++;
-                if(i<n-1 && s[i+1]==' ') return 0; 
-            }
-            if(s[i]=='-' && flag!=1) {
-                ncount++;
-                if(i<n-1 && s[i+1]==' ') return 0; 
-            }
-            if(isdigit(s[i])){
-                flag=1;
-                while(i<n && isdigit(s[i])){
-                    number+=s[i];
-                    i++;
-                }
-                break;
-            }
+        int n=s.length();
+        int i=0,sign=1;
+        long long ans=0;
+        while(i<n&& s[i]==' ') i++;
+        if(i<n &&(s[i]=='+' || s[i]=='-')){
+            if(s[i]=='-') sign=-1;
             i++;
         }
-        // cout<<number<<" "<<pcount<<" "<<ncount<<endl;
-        int f=0;
-        if(ncount==1 && pcount==0) {
-            sign='-';
-            f=1;
-        }
-        if(pcount==1 && ncount==0){
-             sign='+';
-             f=1;
-        }
-        if((ncount>0 || pcount>0) && ncount-pcount!=1 && f==0) return 0;
-        int t=0;
-        while(t<number.size() && number[t]=='0'){
-            t++;
-        }
-        int l=number.size()-t;
-        // cout<<l<<endl;
-        if(sign=='+' && l>10) return INT_MAX;
-        if(sign=='-'&& l>10) return INT_MIN;
-        long long ans=0;
-        long long k=1;
-        for(int i=number.size()-1;i>=t;i--){
-            ans+=(number[i]-'0')*k;
-            k*=10;
-        }
-        // cout<<ans;
-        if(sign=='+' && ans>=INT_MAX) ans=INT_MAX;
-        if(sign=='-' && -ans<=INT_MIN) ans=INT_MIN;
-        if(sign=='-') ans*=-1;
-        return (int)ans;
+       ans= solve(s,i,sign,0);
+       return ans;
     }
 };
